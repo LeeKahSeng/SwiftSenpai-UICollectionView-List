38f140d4ad74b184aee0e4f3758844d57650544a
Lee Kah Seng <kahseng.lee123@gmail.com>
2020-09-30 22:34:33 +0800

    - Setup required files

---
+++ Swift-Senpai-UICollectionView-List/Reload Cell/ReloadValueTypeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReloadValueTypeViewController.swift
+//  Swift-Senpai-UICollectionView-List
+//
+//  Created by Lee Kah Seng on 30/09/2020.
+//
+
+import UIKit
+
+class ReloadValueTypeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
