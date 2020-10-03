516d171089c3db496df6396e819fd8cbec4de925
Lee Kah Seng <kahseng.lee123@gmail.com>
2020-10-03 21:15:52 +0800

    - Implemented reload logic

--- Swift-Senpai-UICollectionView-List/Reload Cell/ReloadReferenceTypeViewController.swift
+++ Swift-Senpai-UICollectionView-List/Reload Cell/ReloadReferenceTypeViewController.swift
@@ -8,22 +8,113 @@
 import UIKit
 
 class ReloadReferenceTypeViewController: UIViewController {
+    
+    class SuperHero: Hashable {
 
+        var name: String
+
+        init(name: String) {
+            self.name = name
+        }
+
+        // MARK: Hashable
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(name)
+        }
+
+        static func == (lhs: ReloadReferenceTypeViewController.SuperHero, rhs: ReloadReferenceTypeViewController.SuperHero) -> Bool {
+            lhs.name == rhs.name
+        }
+    }
+
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, SuperHero>!
+    
+    let heroArray = [
+        SuperHero(name: "Spider-Man"),
+        SuperHero(name: "Superman"),
+        SuperHero(name: "Batman"),
+        SuperHero(name: "Captain America"),
+        SuperHero(name: "Thor"),
+        SuperHero(name: "Wonder Woman"),
+        SuperHero(name: "Iron Man"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = "Reload Reference Type"
+        
+        // MARK: Setup list layout
+        let layoutConfig = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        let listLayout = UICollectionViewCompositionalLayout.list(using: layoutConfig)
+        
+        // MARK: Setup collection view
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: listLayout)
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        
+        // MARK: Make collection view take up the entire view
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 0.0),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0.0),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0.0),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0.0),
+        ])
+        
+        // MARK: Cell registration
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, SuperHero> { (cell, indexPath, item) in
+            
+            // Define how data should be shown using content configuration
+            var content = cell.defaultContentConfiguration()
+            content.text = item.name
+            
+            // Assign content configuration to cell
+            cell.contentConfiguration = content
+        }
+        
+        // MARK: Setup data source
+        dataSource = UICollectionViewDiffableDataSource<Section, SuperHero>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, hero) -> UICollectionViewCell? in
+            
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
+                                                                    for: indexPath,
+                                                                    item: hero)
+            
+            return cell
+        })
+        
+        // MARK: Data source snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, SuperHero>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(heroArray, toSection: .main)
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
+
+}
+
+extension ReloadReferenceTypeViewController: UICollectionViewDelegate {
     
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
+        
+        // Get selected hero using index path
+        guard let selectedHero = dataSource.itemIdentifier(for: indexPath) else {
+            collectionView.deselectItem(at: indexPath, animated: true)
+            return
+        }
+        
+        // Update selectedHero
+        selectedHero.name = selectedHero.name.appending("⭐️")
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Create a new copy of data source snapshot for modification
+        var newSnapshot = dataSource.snapshot()
+        
+        // Reload selectedHero in newSnapshot
+        newSnapshot.reloadItems([selectedHero])
+        
+        // Apply snapshot changes to data source
+        dataSource.apply(newSnapshot)
     }
-    */
 
 }
--- Swift-Senpai-UICollectionView-List/Reload Cell/ReloadValueTypeViewController.swift
+++ Swift-Senpai-UICollectionView-List/Reload Cell/ReloadValueTypeViewController.swift
@@ -9,21 +9,100 @@ import UIKit
 
 class ReloadValueTypeViewController: UIViewController {
 
+    struct SuperHero: Hashable {
+        var name: String
+    }
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, SuperHero>!
+    
+    let heroArray = [
+        SuperHero(name: "Spider-Man"),
+        SuperHero(name: "Superman"),
+        SuperHero(name: "Batman"),
+        SuperHero(name: "Captain America"),
+        SuperHero(name: "Thor"),
+        SuperHero(name: "Wonder Woman"),
+        SuperHero(name: "Iron Man"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = "Reload Value Type"
+        
+        // MARK: Setup list layout
+        let layoutConfig = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        let listLayout = UICollectionViewCompositionalLayout.list(using: layoutConfig)
+        
+        // MARK: Setup collection view
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: listLayout)
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        
+        // MARK: Make collection view take up the entire view
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 0.0),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0.0),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0.0),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0.0),
+        ])
+        
+        // MARK: Cell registration
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, SuperHero> { (cell, indexPath, item) in
+            
+            // Define how data should be shown using content configuration
+            var content = cell.defaultContentConfiguration()
+            content.text = item.name
+            
+            // Assign content configuration to cell
+            cell.contentConfiguration = content
+        }
+        
+        // MARK: Setup data source
+        dataSource = UICollectionViewDiffableDataSource<Section, SuperHero>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, hero) -> UICollectionViewCell? in
+            
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
+                                                                    for: indexPath,
+                                                                    item: hero)
+            
+            return cell
+        })
+        
+        // MARK: Data source snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, SuperHero>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(heroArray, toSection: .main)
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
+
+}
+
+extension ReloadValueTypeViewController: UICollectionViewDelegate {
     
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
+        
+        // Get selected hero using index path
+        guard let selectedHero = dataSource.itemIdentifier(for: indexPath) else {
+            collectionView.deselectItem(at: indexPath, animated: true)
+            return
+        }
 
-    /*
-    // MARK: - Navigation
+        // Create a new copy of selectedHero & update it
+        var updatedHero = selectedHero
+        updatedHero.name = updatedHero.name.appending("⭐️")
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Create a new copy of data source snapshot for modification
+        var newSnapshot = dataSource.snapshot()
+
+        // Update snapshot by inserting new hero item and delete the old one
+        newSnapshot.insertItems([updatedHero], beforeItem: selectedHero)
+        newSnapshot.deleteItems([selectedHero])
+
+        // Apply snapshot changes to data source
+        dataSource.apply(newSnapshot)
     }
-    */
 
 }
