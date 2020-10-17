f00f6aac6b8dec8ec803c5933bc4d1fabf5e0b0d
Lee Kah Seng <kahseng.lee123@gmail.com>
2020-10-17 11:05:56 +0800

    - Declared all required information to populate a list

--- Swift-Senpai-UICollectionView-List/Declarative Header & Footer/System Default/DefaultHeaderFooterViewController.swift
+++ Swift-Senpai-UICollectionView-List/Declarative Header & Footer/System Default/DefaultHeaderFooterViewController.swift
@@ -9,22 +9,132 @@ import UIKit
 
 class DefaultHeaderFooterViewController: UIViewController {
 
+    let modelObjects = [
+        
+        HeaderItem(title: "Communication", symbols: [
+            SFSymbolItem(name: "mic"),
+            SFSymbolItem(name: "message"),
+            SFSymbolItem(name: "message.fill"),
+        ]),
+        
+        HeaderItem(title: "Weather", symbols: [
+            SFSymbolItem(name: "sun.min"),
+            SFSymbolItem(name: "sunset.fill"),
+        ]),
+        
+        HeaderItem(title: "Nature", symbols: [
+            SFSymbolItem(name: "drop.fill"),
+            SFSymbolItem(name: "flame"),
+            SFSymbolItem(name: "bolt.circle.fill"),
+            SFSymbolItem(name: "tortoise.fill"),
+        ]),
+    ]
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<HeaderItem, SFSymbolItem>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // MARK: Create list layout
+        var layoutConfig = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        layoutConfig.headerMode = .supplementary
+        layoutConfig.footerMode = .supplementary
+        let listLayout = UICollectionViewCompositionalLayout.list(using: layoutConfig)
+        
+        // MARK: Configure collection view
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: listLayout)
+        view.addSubview(collectionView)
+
+        // Make collection view take up the entire view
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 0.0),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0.0),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0.0),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0.0),
+        ])
+        
+        // MARK: Cell registration
+        let symbolCellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, SFSymbolItem> {
+            (cell, indexPath, symbolItem) in
+            
+            // Configure cell content
+            var configuration = cell.defaultContentConfiguration()
+            configuration.image = symbolItem.image
+            configuration.text = symbolItem.name
+            cell.contentConfiguration = configuration
+        }
+        
+        // MARK: Supplementary view registration
+        let headerRegistration = UICollectionView.SupplementaryRegistration
+        <UICollectionViewListCell>(elementKind: UICollectionView.elementKindSectionHeader) {
+            [unowned self] (headerView, elementKind, indexPath) in
+            
+            let headerItem = self.modelObjects[indexPath.section]
+            
+            // Configure header view content based on headerItem
+            var configuration = headerView.defaultContentConfiguration()
+            configuration.text = headerItem.title
+            configuration.textProperties.font = .boldSystemFont(ofSize: 16)
+            configuration.textProperties.color = .systemBlue
+            configuration.directionalLayoutMargins = .init(top: 20.0, leading: 0.0, bottom: 10.0, trailing: 0.0)
+            headerView.contentConfiguration = configuration
+        }
+        
+        let footerRegistration = UICollectionView.SupplementaryRegistration
+        <UICollectionViewListCell>(elementKind: UICollectionView.elementKindSectionHeader) {
+            [unowned self] (footerView, elementKind, indexPath) in
+            
+            let headerItem = self.modelObjects[indexPath.section]
+            let symbolCount = headerItem.symbols.count
+            
+            var configuration = footerView.defaultContentConfiguration()
+            configuration.text = "Symbol count: \(symbolCount)"
+            footerView.contentConfiguration = configuration
+        }
+        
+        // MARK: Initialize data source
+        dataSource = UICollectionViewDiffableDataSource<HeaderItem, SFSymbolItem>(collectionView: collectionView) {
+            (collectionView, indexPath, symbolItem) -> UICollectionViewCell? in
+            
+            // Dequeue symbol cell
+            let cell = collectionView.dequeueConfiguredReusableCell(using: symbolCellRegistration,
+                                                                    for: indexPath,
+                                                                    item: symbolItem)
+            return cell
+        }
+        
+        // Define supplementaryViewProvider
+        dataSource.supplementaryViewProvider = { [unowned self]
+            (collectionView, elementKind, indexPath) -> UICollectionReusableView? in
+            
+            if elementKind == UICollectionView.elementKindSectionHeader {
+                
+                // Dequeue header view
+                return self.collectionView.dequeueConfiguredReusableSupplementary(
+                    using: headerRegistration, for: indexPath)
+                
+            } else {
+                
+                // Dequeue footer view
+                return self.collectionView.dequeueConfiguredReusableSupplementary(
+                    using: footerRegistration, for: indexPath)
+            }
+        }
+        
+        // MARK: Setup snapshots
+        var dataSourceSnapshot = NSDiffableDataSourceSnapshot<HeaderItem, SFSymbolItem>()
+
+        // Create collection view section based on number of HeaderItem in modelObjects
+        dataSourceSnapshot.appendSections(modelObjects)
+
+        // Loop through each header item to append symbols to their respective section
+        for headerItem in modelObjects {
+            dataSourceSnapshot.appendItems(headerItem.symbols, toSection: headerItem)
+        }
+        
+        dataSource.apply(dataSourceSnapshot)
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
