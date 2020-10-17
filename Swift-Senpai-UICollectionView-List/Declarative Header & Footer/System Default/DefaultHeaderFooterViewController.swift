56a3d8a1c0425f2152cd0d6f0011f40a4e40fa58
Lee Kah Seng <kahseng.lee123@gmail.com>
2020-10-17 10:10:30 +0800

    - Added required class

---
+++ Swift-Senpai-UICollectionView-List/Declarative Header & Footer/System Default/DefaultHeaderFooterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DefaultHeaderFooterViewController.swift
+//  Swift-Senpai-UICollectionView-List
+//
+//  Created by Lee Kah Seng on 17/10/2020.
+//
+
+import UIKit
+
+class DefaultHeaderFooterViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
