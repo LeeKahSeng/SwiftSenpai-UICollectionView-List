8933721ddeab8b8c254960f190017e4efcd3cca8
Lee Kah Seng <kahseng.lee123@gmail.com>
2020-10-27 23:05:41 +0800

    - Added view controller

---
+++ Swift-Senpai-UICollectionView-List/Custom Header & Footer/CustomHeaderFooterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomHeaderFooterViewController.swift
+//  Swift-Senpai-UICollectionView-List
+//
+//  Created by Lee Kah Seng on 27/10/2020.
+//
+
+import UIKit
+
+class CustomHeaderFooterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
