71091b7166e0f12067daaf33ebc9aa05bba5d287
Lee Kah Seng <kahseng.lee123@gmail.com>
2021-01-01 21:58:59 +0800

    Implemented date picker replica layout

---
+++ Swift-Senpai-UICollectionView-List/Date Picker Replica/DatePickerContentView.swift
@@ -0,0 +1,74 @@
+//
+//  DatePickerContentView.swift
+//  Swift-Senpai-UICollectionView-List
+//
+//  Created by Lee Kah Seng on 01/01/2021.
+//
+
+import Foundation
+import UIKit
+
+class DatePickerContentView: UIView, UIContentView {
+
+    let datePicker = UIDatePicker()
+    private var currentConfiguration: DatePickerContentConfiguration!
+    var configuration: UIContentConfiguration {
+        get {
+            currentConfiguration
+        }
+        set {
+            guard let newConfiguration = newValue as? DatePickerContentConfiguration else {
+                return
+            }
+      
+            apply(configuration: newConfiguration)
+        }
+    }
+    
+    init(configuration: DatePickerContentConfiguration) {
+        super.init(frame: .zero)
+        
+        // Create the content view UI
+        setupAllViews()
+        
+        // Apply the configuration (set data to UI elements / define custom content view appearance)
+        apply(configuration: configuration)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK:- Private functions
+private extension DatePickerContentView {
+    
+    private func setupAllViews() {
+        
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        addSubview(datePicker)
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            datePicker.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            datePicker.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            datePicker.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+            datePicker.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
+        ])
+    }
+    
+    private func apply(configuration: DatePickerContentConfiguration) {
+    
+        // Only apply configuration if new configuration and current configuration are not the same
+        guard currentConfiguration != configuration else {
+            return
+        }
+        
+        // Replace current configuration with new configuration
+        currentConfiguration = configuration
+        
+        // Set date picker's date
+        datePicker.date = configuration.date!
+    }
+}
---
+++ Swift-Senpai-UICollectionView-List/Date Picker Replica/DatePickerReplicaViewController.swift
@@ -0,0 +1,119 @@
+//
+//  DatePickerReplicaViewController.swift
+//  Swift-Senpai-UICollectionView-List
+//
+//  Created by Lee Kah Seng on 01/01/2021.
+//
+
+import UIKit
+
+enum DatePickerItem: Hashable {
+    case header(Date)
+    case picker(Date)
+}
+
+class DatePickerReplicaViewController: UIViewController {
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, DatePickerItem>!
+    var snapshot: NSDiffableDataSourceSnapshot<Section, DatePickerItem>!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Date Picker Replica"
+        
+        // Create list layout
+        let layoutConfig = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        let listLayout = UICollectionViewCompositionalLayout.list(using: layoutConfig)
+        
+        // Create collection view with list layout
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: listLayout)
+        view.addSubview(collectionView)
+        
+        // Make collection view take up the entire view
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 0.0),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0.0),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0.0),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0.0),
+        ])
+        
+        let headerCellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, DatePickerItem> {
+            (cell, indexPath, item) in
+            
+            if case let DatePickerItem.header(date) = item {
+                
+                // Show date on cell
+                var content = cell.defaultContentConfiguration()
+                content.text = "\(date)"
+                cell.contentConfiguration = content
+            }
+            
+            // Add outline disclosure accessory
+            // With this accessory, the header cell's children will expand / collapse when the header cell is tapped.
+            let headerDisclosureOption = UICellAccessory.OutlineDisclosureOptions(style: .header)
+            cell.accessories = [.outlineDisclosure(options:headerDisclosureOption)]
+        }
+        
+        let pickerCellRegistration = UICollectionView.CellRegistration<DatePickerCell, DatePickerItem> { (cell, indexPath, item) in
+            
+            // Set date to date picker cell
+            if case let DatePickerItem.picker(date) = item {
+                cell.date = date
+            }
+        }
+        
+        // Define data source
+        dataSource = UICollectionViewDiffableDataSource<Section, DatePickerItem>(collectionView: collectionView) {
+            (collectionView, indexPath, datePickerItem) -> UICollectionViewCell? in
+            
+            switch datePickerItem {
+            case .header(_):
+                
+                // Dequeue header cell
+                let cell = collectionView.dequeueConfiguredReusableCell(using: headerCellRegistration,
+                                                                        for: indexPath,
+                                                                        item: datePickerItem)
+                return cell
+                
+            case .picker(_):
+                
+                // Dequeue symbol cell
+                let cell = collectionView.dequeueConfiguredReusableCell(using: pickerCellRegistration,
+                                                                        for: indexPath,
+                                                                        item: datePickerItem)
+                return cell
+            }
+        }
+        
+        var dataSourceSnapshot = NSDiffableDataSourceSnapshot<Section, DatePickerItem>()
+        
+        // Define collection view section
+        dataSourceSnapshot.appendSections([.main])
+        dataSource.apply(dataSourceSnapshot)
+        
+        // Create a section snapshot
+        var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<DatePickerItem>()
+        
+        // Show current date in date picker
+        let now = Date()
+        
+        // Define header and set it as parent
+        let header = DatePickerItem.header(now)
+        sectionSnapshot.append([header])
+        
+        // Define picker and set it as child of `header`
+        let picker = DatePickerItem.picker(now)
+        sectionSnapshot.append([picker], to: header)
+        
+        // Expand this section by default
+        sectionSnapshot.expand([header])
+        
+        // Apply section snapshot to main section
+        dataSource.apply(sectionSnapshot, to: .main, animatingDifferences: false)
+        
+    }
+    
+}
